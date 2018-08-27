5d2d0762c9dd459a2f3e0400dba6064c398f025b
MoQinChao <moqinchao@outlook.com>
2018-08-27 16:07:46 +0800

    jest setting
    使用 jest 测试
    重新设计user reducer 初始化状态

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,9 +1,16 @@
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
 
-export function CreateUserStatus(actionType, status) {
+export function CreateSignInAction() {
   return {
-    type: actionType,
-    status,
+    type: SIGN_IN,
+    status: SIGN_IN,
+  };
+}
+
+export function CreateSignOutAction() {
+  return {
+    type: SIGN_OUT,
+    status: SIGN_OUT,
   };
 }
